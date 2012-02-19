b01a715957697e2482a250306f9c1ecaca7c9237
Onezino Gabriel <heat2k4@hotmail.com>
2012-02-19 19:16:03 -0300

    Define Onezino como administrador do projeto.

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Onezino Gabriel', 'heat2k4@hotmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -115,6 +115,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    'tastypie',
 )
 
 # A sample logging configuration. The only tangible logging
