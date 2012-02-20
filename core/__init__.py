91fee5521831bdc7dd4f18ee60ed1664844b728e
Onezino Gabriel <heat2k4@hotmail.com>
2012-02-20 04:27:21 -0300

    Adiciona modelos Author e Group ao core de desenv.

---
+++ core/__init__.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from models import Group, Author
+
+admin.site.register(Group)
+admin.site.register(Author)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 # -*- coding: utf-8 -*-
+from django.contrib.auth.models import User
 
 from django.db import models
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=60)
+    createon = models.DateField(auto_now_add=True)
+    leader = models.ForeignKey('Author', blank=True, null=True)
+
+class Author(User):
+    mugen_group = models.ForeignKey('Group')
